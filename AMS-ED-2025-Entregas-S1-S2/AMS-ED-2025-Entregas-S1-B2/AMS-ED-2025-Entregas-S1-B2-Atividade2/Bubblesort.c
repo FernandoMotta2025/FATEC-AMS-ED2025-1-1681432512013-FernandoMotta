dab39ca2664ae2be656bf05f1652021d3693fd47
FernandoMotta2025 <fernando.fernandes22@fatec.sp.gov.br>
2025-05-06 17:50:57 -0300

    Add files via upload

---
+++ AMS-ED-2025-Entregas-S1-S2/AMS-ED-2025-Entregas-S1-B2/AMS-ED-2025-Entregas-S1-B2-Atividade2/Bubblesort.c
@@ -0,0 +1,44 @@
+/*   FATEC-São Caetano do Sul                 Estrutura de Dados                    */
+/*                         Id da Atividade: S1-B2-2                                 */
+/*             Objetivo: <<   utilização do Bubblesort    >>                        */
+/*                                                                                  */
+/*                                            Autor: Fernando Motta Fernandes       */
+/*                                                                   Data:02/04/2025*/
+
+#include <stdio.h>
+
+int main() {
+    int quant, i, j, temp;
+
+    // Pede a quantidade de elementos
+    printf("Digite a quantidade de elementos: ");
+    scanf("%d", &quant);
+
+    int arr[quant];
+
+    // Lê os elementos
+    for(i = 0; i < quant; i++) {
+        printf("Digite o elemento %d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+
+    
+    for(i = 0; i < quant - 1; i++) {
+        for(j = 0; j < quant - i - 1; j++) {
+            
+            if(arr[j] > arr[j + 1]) {
+                // Troca os elementos
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    // Exibe o array em ordem decrescente
+    printf("Array : ");
+    for(i = 0; i < quant; i++) {
+        printf("%d  ", arr[i]);
+    }
+    return 0;
+}
